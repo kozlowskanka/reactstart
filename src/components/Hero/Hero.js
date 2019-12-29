68c1ef693ede25f2e9ff57a009749990fb566fc7
Anka Kozłowska <kozlowskanka@gmail.com>
2019-12-29 18:14:11 +0100

    Hero component set as an arrow function

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import styles from './Hero.scss';
 
-class Hero extends React.Component {
-  render() {
-    return (
-      <header className={styles.component}>
+const Hero = () => (
+    <header className={styles.component}>
         <h2 className={styles.title}>To do things</h2>
         <img className={styles.image}
             src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"></img>
-      </header>
-    )
-  }
-}
+    </header>
+)
 
 export default Hero;
