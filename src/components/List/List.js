463ac83ac93dccdc077e368eefbd89f518ac03fa
Anka Kozłowska <kozlowskanka@gmail.com>
2020-01-11 13:11:48 +0100

    Comtainer component added

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './List.scss';
+import Container from '../Container/Container';
 import Hero from '../Hero/Hero';
 import Creator from '../Creator/Creator';
 import Column from '../Column/ColumnContainer';
@@ -25,15 +26,17 @@ class List extends React.Component {
     const {title, image, description, columns, addColumn} = this.props;
     return (
       <section className={styles.component}>
-        <Hero titleText={title} heroImage={image} />
-        <div className={styles.description}>
-          {ReactHtmlParser(description)}
-        </div>
-        <div className={styles.columns}>
-          {columns.map(columnData => (
-            <Column key={columnData.id} {...columnData} />
-          ))}
-        </div>
+        <Container>
+          <Hero titleText={title} heroImage={image} />
+          <div className={styles.description}>
+            {ReactHtmlParser(description)}
+          </div>
+          <div className={styles.columns}>
+            {columns.map(columnData => (
+              <Column key={columnData.id} {...columnData} />
+            ))}
+          </div>
+        </Container>
         <div className={styles.creator}>
           <Creator text={settings.columnCreatorText} action={addColumn}/>
         </div>
